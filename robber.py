a6390dfae695b531a10a37eae2b160f6edb205ab
Zenash <109104482+Akisoy-Zenash@users.noreply.github.com>
2023-10-20 12:24:44 +0100

    add robber.py

---
+++ robber.py
@@ -0,0 +1,17 @@
+houses = [2, 16 , 9, 5, 20 , 10, 1]
+paire = 0
+impaire = 0
+
+
+for idx, i in enumerate(houses):
+    if idx % 2 == 0:
+        paire += i
+    else:
+        impaire += i
+
+if paire > impaire:
+    print(f"The max is {paire} starting with the first house and going on one by one")
+elif paire < impaire:
+    print(f"The max is {imapaire} starting with the second house and going on one by one")
+else:
+    print(f"The max is {paire}, it doesn't matter where u start just go on one by one")
